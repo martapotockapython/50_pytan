07d704c274cf36704d3c6b2f12717ef1cbcc25e2
Marta Potocka <marta.m.slusarczyk@gmail.com>
2020-05-15 00:54:01 +0200

    update comments

--- 41_wypisanie_zawartosci_katalogu.py
+++ 41_wypisanie_zawartosci_katalogu.py
@@ -7,17 +7,12 @@
 import os
 
 def wypisz_zawartosc_katalogu(sciezka_do_katalogu):
-    # dla każdego elementu w katalogu pod adresem 'sciezka_do_katalogu'
-    for element in os.listdir(sciezka_do_katalogu):
-    # przy uzyciu os.path.join utworz sciezke do tego elementu
-        sciezka_do_elementu = os.path.join(sciezka_do_katalogu, element)
-    # jeśli element jest również katalogiem
-        if os.path.isdir(sciezka_do_elementu):
-    # uruchom rekurencyjnie funkcję wypisz_zawartosc_katalogu z adresem elementu jako argumentem
-            wypisz_zawartosc_katalogu(sciezka_do_elementu)
-    # w przeciwnym wypadku (czyli element nie jest katalogiem)
-        else:
-    # wypisz element
-            print(sciezka_do_elementu)
+
+    for element in os.listdir(sciezka_do_katalogu):        # dla każdego elementu w katalogu pod adresem 'sciezka_do_katalogu'
+        sciezka_do_elementu = os.path.join(sciezka_do_katalogu, element) # przy uzyciu os.path.join utworz sciezke do tego elementu
+        if os.path.isdir(sciezka_do_elementu):             # jeśli element jest również katalogiem
+            wypisz_zawartosc_katalogu(sciezka_do_elementu) # uruchom rekurencyjnie funkcję wypisz_zawartosc_katalogu z adresem elementu jako argumentem
+        else:                                              # w przeciwnym wypadku (czyli element nie jest katalogiem)
+            print(sciezka_do_elementu)                     # wypisz element
 
 wypisz_zawartosc_katalogu(r"C:\Udemy\50 pytań\kod\testowy")
